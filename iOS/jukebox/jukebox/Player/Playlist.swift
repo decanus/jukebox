f6697913493106ead9bcd44ca7875c5e073b49b4
decanus <dean.eigenmann@icloud.com>
2016-05-09 10:17:02 +0200

    More files, background audio

---
+++ iOS/jukebox/jukebox/Player/Playlist.swift
@@ -0,0 +1,19 @@
+//
+//  Playlist.swift
+//  jukebox
+//
+//  Created by Dean Eigenmann on 09/05/16.
+//  Copyright © 2016 jukebox. All rights reserved.
+//
+
+import Foundation
+
+class Playlist {
+    
+    private var tracks = [Track]()
+    
+    func getTracks() -> [Track] {
+        return tracks
+    }
+    
+}
