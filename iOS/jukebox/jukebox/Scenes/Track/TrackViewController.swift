0b50dd12ca9b1bea332a763509491da494329d0d
decanus <dean.eigenmann@icloud.com>
2016-05-09 09:55:20 +0200

    Added TrackViewController

---
+++ iOS/jukebox/jukebox/Scenes/Track/TrackViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TrackViewController.swift
+//  jukebox
+//
+//  Created by Dean Eigenmann on 09/05/16.
+//  Copyright © 2016 jukebox. All rights reserved.
+//
+
+import UIKit
+
+class TrackViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
