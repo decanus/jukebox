8e9f259780a593f6b68f15265dfb6fe4c624e410
decanus <dean.eigenmann@icloud.com>
2016-05-09 10:45:50 +0200

    coder shit

--- iOS/jukebox/jukebox/Scenes/Track/TrackViewController.swift
+++ iOS/jukebox/jukebox/Scenes/Track/TrackViewController.swift
@@ -17,6 +17,10 @@ class TrackViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
