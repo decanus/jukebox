ea840ea1bed5f6617e5631fcf9a0d97c577b2e05
decanus <dean.eigenmann@icloud.com>
2016-05-09 10:42:13 +0200

    init

--- iOS/jukebox/jukebox/Scenes/Player/PlayerViewController.swift
+++ iOS/jukebox/jukebox/Scenes/Player/PlayerViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class PlayerViewController: UIViewController {
     
+    private let player: Player
+    
+    init(player: Player) {
+        self.player = player
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
