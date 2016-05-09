c4c4f6e623dcaf0dd67560cbe8db22addd9d4666
decanus <dean.eigenmann@icloud.com>
2016-05-09 11:01:29 +0200

    added track object

---
+++ iOS/jukebox/jukebox/Player/Tracks/YoutubeTrack.swift
@@ -0,0 +1,25 @@
+//
+//  YoutubeTrack.swift
+//  jukebox
+//
+//  Created by Dean Eigenmann on 09/05/16.
+//  Copyright © 2016 jukebox. All rights reserved.
+//
+
+import Foundation
+
+class YoutubeTrack: Track {
+    
+    func getID() -> String {
+        return ""
+    }
+    
+    func getDuration() -> NSTimeInterval {
+        return 0
+    }
+    
+    func getPlatform() -> Platform {
+        return .Youtube
+    }
+    
+}
