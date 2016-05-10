106f356b9fa11f071b6a125ec21cb7164e00cbf7
decanus <dean.eigenmann@icloud.com>
2016-05-10 08:49:54 +0200

    Added pods

---
+++ iOS/jukebox/jukebox/Player/Players/YoutubePlayer.swift
@@ -0,0 +1,22 @@
+//
+//  YoutubePlayer.swift
+//  jukebox
+//
+//  Created by Dean Eigenmann on 10/05/16.
+//  Copyright © 2016 jukebox. All rights reserved.
+//
+
+import Foundation
+import youtube_ios_player_helper
+
+class YoutubePlayer: PlayerProtocol {
+    
+    func pause() {
+        
+    }
+    
+    func play() {
+        
+    }
+    
+}
