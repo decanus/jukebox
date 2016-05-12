845fb51df3e8c973ae88bc1d3fbf1e9775e73d29
decanus <dean.eigenmann@icloud.com>
2016-05-12 22:59:43 +0200

    added

--- iOS/jukebox/jukebox/Scenes/Player/PlayerViewController.swift
+++ iOS/jukebox/jukebox/Scenes/Player/PlayerViewController.swift
@@ -25,21 +25,43 @@ class PlayerViewController: UIViewController {
         super.viewDidLoad()
         tabBarItem.title = "Player"
         navigationController?.navigationBarHidden = true
+        UIApplication.sharedApplication().statusBarStyle = .LightContent
+        
 
         view.backgroundColor = UIColor.blackColor()
         
-        let pause = UIButton(frame: CGRect(x: 10, y: 100, width: 50, height: 25))
+        // @todo, create pause button class which includes this
+        let circlePath = UIBezierPath(
+            arcCenter: CGPoint(x: (view.frame.size.width / 2), y: 501),
+            radius: CGFloat(40),
+            startAngle: CGFloat(0),
+            endAngle:CGFloat(M_PI * 2),
+            clockwise: true
+        )
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circlePath.CGPath
+        shapeLayer.fillColor = UIColor(red: 155 / 255, green: 80 / 255, blue: 186 / 255, alpha: 1).CGColor
+        shapeLayer.strokeColor = UIColor(red: 155 / 255, green: 80 / 255, blue: 186 / 255, alpha: 1).CGColor
+        shapeLayer.lineWidth = 3.0
+        view.layer.addSublayer(shapeLayer)
+        
+        let pause = UIButton(frame: CGRect(x: (view.frame.size.width / 2) - 40, y: 501 - (80 / 2), width: 80, height: 80))
         pause.addTarget(self, action: #selector(PlayerViewController.pause), forControlEvents: .TouchUpInside)
-        pause.backgroundColor = UIColor.redColor()
+        pause.setImage(UIImage(named: "play"), forState: .Normal)
+        pause.imageView?.frame.size = CGSize(width: 20, height: 24)
+        pause.imageView?.contentMode = .Center
         view.addSubview(pause)
         
+        let previous = UIButton(frame: CGRect(x: (view.frame.size.width / 2) - (40 + 40 + 31), y: 501 - (21 / 2), width: 31, height: 21))
+        previous.addTarget(self, action: #selector(PlayerViewController.previous), forControlEvents: .TouchUpInside)
+        previous.setImage(UIImage(named: "previous"), forState: .Normal)
+        view.addSubview(previous)
         
-        let next = UIButton(frame: CGRect(x: 70, y: 100, width: 50, height: 25))
+        let next = UIButton(frame: CGRect(x: (view.frame.size.width / 2) + 40 + 40, y: 501 - (21 / 2), width: 31, height: 21))
         next.addTarget(self, action: #selector(PlayerViewController.next), forControlEvents: .TouchUpInside)
-        next.backgroundColor = UIColor.redColor()
-        next.setTitle("Next", forState: .Normal)
+        next.setImage(UIImage(named: "next"), forState: .Normal)
         view.addSubview(next)
-
     }
     
     // @move to presenter & interactor
@@ -52,6 +74,10 @@ class PlayerViewController: UIViewController {
         
         player.play()
 
+    }
+    
+    @objc func previous() {
+        player.previous()
     }
     
     @objc func next() {
