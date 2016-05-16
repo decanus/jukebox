0d391897772116c0ecda551565a2194643b0194a
decanus <dean.eigenmann@icloud.com>
2016-05-17 00:43:03 +0200

    removed comments

--- iOS/jukebox/jukebox/Scenes/Search/SearchViewController.swift
+++ iOS/jukebox/jukebox/Scenes/Search/SearchViewController.swift
@@ -39,7 +39,6 @@ class SearchViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 }
