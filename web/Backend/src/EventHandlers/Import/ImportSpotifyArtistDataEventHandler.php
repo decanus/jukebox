6dc6c8f2fc8624aa55801e3e43e524ef614b7c34
decanus <dean.eigenmann@icloud.com>
2016-05-17 14:15:16 +0200

    added spotify service

--- web/Backend/src/EventHandlers/Import/ImportSpotifyArtistDataEventHandler.php
+++ web/Backend/src/EventHandlers/Import/ImportSpotifyArtistDataEventHandler.php
@@ -4,9 +4,19 @@ namespace Jukebox\Backend\EventHandlers\Import
 {
 
     use Jukebox\Backend\EventHandlers\EventHandlerInterface;
+    use Jukebox\Backend\Events\ImportSpotifyArtistDataEvent;
 
     class ImportSpotifyArtistDataEventHandler implements EventHandlerInterface
     {
+        /**
+         * @var ImportSpotifyArtistDataEvent
+         */
+        private $event;
+
+        public function __construct(ImportSpotifyArtistDataEvent $event)
+        {
+            $this->event = $event;
+        }
 
         public function execute()
         {
