44871b5836465ba105aad4e577ccc7727ab7a6a3
decanus <dean.eigenmann@icloud.com>
2016-05-17 14:09:04 +0200

    started first event

---
+++ web/Backend/src/EventHandlers/Import/ImportSpotifyArtistDataEventHandler.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Jukebox\Backend\EventHandlers\Import
+{
+
+    use Jukebox\Backend\EventHandlers\EventHandlerInterface;
+
+    class ImportSpotifyArtistDataEventHandler implements EventHandlerInterface
+    {
+
+        public function execute()
+        {
+            // TODO: Implement execute() method.
+        }
+    }
+
+}
---
+++ web/Backend/src/Events/ImportSpotifyArtistDataEvent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Jukebox\Backend\Events
+{
+
+    use Jukebox\Framework\Events\EventInterface;
+
+    class ImportSpotifyArtistDataEvent implements EventInterface
+    {
+        /**
+         * @var string
+         */
+        private $artistId;
+
+        public function __construct(string $artistId)
+        {
+            $this->artistId = $artistId;
+        }
+
+        public function getArtistId(): string
+        {
+            return $this->artistId;
+        }
+
+        public function getName(): string
+        {
+            return 'ImportSpotifyArtistData';
+        }
+    }
+}
