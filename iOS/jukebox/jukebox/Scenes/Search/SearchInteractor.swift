afc42c599514ad48931923ddf3fc86f8cffc42c7
decanus <dean.eigenmann@icloud.com>
2016-05-17 06:49:17 +0200

    line break

--- iOS/jukebox/jukebox/Scenes/Search/SearchInteractor.swift
+++ iOS/jukebox/jukebox/Scenes/Search/SearchInteractor.swift
@@ -10,7 +10,11 @@ import Foundation
 
 class SearchInteractor: NSObject, SearchViewControllerOutput {
     
-    var tracks: [Track]? = [YoutubeTrack(id: "Bag1gUxuU0g", duration: 287), YoutubeTrack(id: "Bag1gUxuU0g", duration: 287), YoutubeTrack(id: "Bag1gUxuU0g", duration: 287)]
+    var tracks: [Track]? = [
+        YoutubeTrack(id: "Bag1gUxuU0g", duration: 287),
+        YoutubeTrack(id: "Bag1gUxuU0g", duration: 287),
+        YoutubeTrack(id: "Bag1gUxuU0g", duration: 287)
+    ]
     
     func trackForIndex(index: Int) -> Track {
         return YoutubeTrack(id: "Bag1gUxuU0g", duration: 287)
