8d7081b95494f1ecae9776e70da5ce69b5f80abb
decanus <dean.eigenmann@icloud.com>
2016-05-18 17:23:15 +0200

    added db backend

---
+++ web/Framework/src/Backends/SQL/PostgresDatabaseBackend.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Jukebox\Framework\Backends
+{
+    class PostgresDatabaseBackend
+    {
+        /**
+         * @var PDO
+         */
+        private $PDO;
+
+        public function __construct(PDO $PDO)
+        {
+            $this->PDO = $PDO;
+        }
+
+        public function fetchAll(string $sql, array $parameters = []): mixed
+        {
+            /** @var $statement \PDOStatement */
+            $statement = $this->PDO->prepare($sql);
+            $statement->execute($parameters);
+
+            return $this->fetchAll(\PDO::FETCH_ASSOC);
+        }
+
+        public function insert(string $sql, array $parameters = []): bool
+        {
+            /** @var $statement \PDOStatement */
+            $statement = $this->PDO->prepare($sql);
+            return $statement->execute($parameters);
+        }
+    }
+}
