aab3cf07462bcaf4ecfbf04f0631e2dd7eec0de3
decanus <dean.eigenmann@icloud.com>
2016-05-18 07:39:36 +0200

    extended

--- web/Backend/src/EventHandlers/Import/InitialVevoArtistsImportEventHandler.php
+++ web/Backend/src/EventHandlers/Import/InitialVevoArtistsImportEventHandler.php
@@ -22,17 +22,31 @@ namespace Jukebox\Backend\EventHandlers\Import
         public function execute()
         {
             try {
-                $artists = $this->vevo->getArtists();
+                $artists = $this->vevo->getArtists()->getDecodedJsonResponse();
 
-                $data = $artists->getDecodedJsonResponse();
-
-                for ($i = 2; $i <= $data['paging']['pages']; $i++) {
-                    var_dump($this->vevo->getArtists($i));
+                foreach ($artists['artists'] as $artist) {
+                    $this->handleArtist($artist);
                 }
                 
+
+                $pages = $artists['paging']['pages'];
+                for ($i = 2; $i <= $pages; $i++) {
+                    $artists = $this->vevo->getArtists($i)->getDecodedJsonResponse()['artists'];
+
+                    foreach ($artists as $artist) {
+                        $this->handleArtist($artist);
+                    }
+                }
+
             } catch (\Exception $e) {
+                var_dump($e->getMessage());
                 // @todo handle
             }
+        }
+
+        public function handleArtist(array $artist)
+        {
+            var_dump($artist['name']);
         }
     }
 }
