d7d30c114ff9ad9a05fd92287e84dee4d02cf0aa
decanus <dean.eigenmann@icloud.com>
2016-05-18 19:39:07 +0200

    updated

--- web/Backend/src/Commands/InsertVevoArtistCommand.php
+++ web/Backend/src/Commands/InsertVevoArtistCommand.php
@@ -4,6 +4,7 @@ namespace Jukebox\Backend\Commands
 {
 
     use Jukebox\Framework\Backends\PostgreDatabaseBackend;
+    use Jukebox\Framework\ValueObjects\Uri;
 
     class InsertVevoArtistCommand
     {
@@ -17,11 +18,28 @@ namespace Jukebox\Backend\Commands
             $this->postgreDatabaseBackend = $postgreDatabaseBackend;
         }
 
-        public function execute($artist, $urlSafeName): bool
+        public function execute(
+            string $artist,
+            string $urlSafeName,
+            Uri $officialWebsite = null,
+            string $twitter = null,
+            Uri $facebook = null,
+            Uri $itunes = null,
+            Uri $amazon = null
+        ): bool
         {
             return $this->postgreDatabaseBackend->insert(
-                'INSERT INTO artists (name, urlSafeName, isVevo) VALUES (:name, :urlSafeName, :isVevo)',
-                [':name' => $artist, ':urlSafeName' => $urlSafeName, ':isVevo' => true]
+                'INSERT INTO artists (name, urlSafeName, isVevo, officialWebsite, twitter, facebook, itunes, amazon) VALUES (:name, :urlSafeName, :isVevo, :officialWebsite, :twitter, :facebook, :itunes, :amazon)',
+                [
+                    ':name' => $artist,
+                    ':urlSafeName' => $urlSafeName,
+                    ':isVevo' => true,
+                    ':officialWebsite' => (string) $officialWebsite,
+                    ':twitter' => $twitter,
+                    ':facebook' => (string) $facebook,
+                    ':itunes' => (string) $itunes,
+                    ':amazon' => (string) $amazon
+                ]
             );
         }
     }
