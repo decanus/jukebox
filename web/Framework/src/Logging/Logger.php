39bfc22fe43309408efbc4e73b9dac90a0b6ae3a
decanus <dean.eigenmann@icloud.com>
2016-05-19 18:35:30 +0200

    More log levels

--- web/Framework/src/Logging/Logger.php
+++ web/Framework/src/Logging/Logger.php
@@ -5,9 +5,14 @@ namespace Jukebox\Framework\Logging
 
     use Jukebox\Framework\Logging\Loggers\AbstractLogger;
     use Jukebox\Framework\Logging\Loggers\LoggerInterface;
+    use Jukebox\Framework\Logging\Logs\AlertLog;
     use Jukebox\Framework\Logging\Logs\CriticalLog;
+    use Jukebox\Framework\Logging\Logs\DebugLog;
     use Jukebox\Framework\Logging\Logs\EmergencyLog;
+    use Jukebox\Framework\Logging\Logs\ErrorLog;
     use Jukebox\Framework\Logging\Logs\LogInterface;
+    use Jukebox\Framework\Logging\Logs\NoticeLog;
+    use Jukebox\Framework\Logging\Logs\WarningLog;
 
     class Logger implements LoggerInterface
     {
@@ -34,27 +39,27 @@ namespace Jukebox\Framework\Logging
 
         public function alert(\Throwable $e)
         {
-
+            $this->log(new AlertLog($e));
         }
 
         public function error(\Throwable $e)
         {
-
+            $this->log(new ErrorLog($e));
         }
 
         public function warning(\Throwable $e)
         {
-
+            $this->log(new WarningLog($e));
         }
 
         public function notice(\Throwable $e)
         {
-
+            $this->log(new NoticeLog($e));
         }
 
         public function debug(\Throwable $e)
         {
-
+            $this->log(new DebugLog($e));
         }
         
         public function log(LogInterface $log)
---
+++ web/Framework/src/Logging/Logs/DebugLog.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Jukebox\Framework\Logging\Logs
+{
+    class DebugLog implements LogInterface
+    {
+
+        public function getLog(): array
+        {
+            // TODO: Implement getLog() method.
+        }
+
+        public function getMessage(): string
+        {
+            // TODO: Implement getMessage() method.
+        }
+    }
+}
