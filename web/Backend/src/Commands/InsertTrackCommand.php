3f0a4438bd832213c221385488eb3071fc33a7f3
decanus <dean.eigenmann@icloud.com>
2016-05-22 16:04:39 +0200

    updated command

--- web/Backend/src/Commands/InsertTrackCommand.php
+++ web/Backend/src/Commands/InsertTrackCommand.php
@@ -4,9 +4,18 @@ namespace Jukebox\Backend\Commands
 {
     class InsertTrackCommand extends AbstractDatabaseBackendCommand
     {
-        public function execute()
+        public function execute(
+            int $duration,
+            string $title,
+            string $youtubeId = null,
+            string $vevoId = null,
+            bool $isLive = false
+        ): bool
         {
-
+            return $this->getDatabaseBackend()->insert(
+                'INSERT INTO tracks (duration, title, youtube_id, vevo_id, is_live) VALUES (:duration, :title, :youtube_id, :vevo_id, :is_live)',
+                [':duration' => $duration, ':title' => $title, ':youtube_id' => $youtubeId, ':vevo_id' => $vevoId, ':is_live' => $isLive]
+            );
         }
     }
 }
