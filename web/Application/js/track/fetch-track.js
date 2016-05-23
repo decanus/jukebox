a8ebb4391b277646de8c009187493e3d226bd72a
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-05-23 20:57:03 +0200

    disable nginx caching in dev, add yonce

--- web/Application/js/track/fetch-track.js
+++ web/Application/js/track/fetch-track.js
@@ -14,11 +14,9 @@ export function fetchTrack(trackId) {
   switch(trackId) {
     case 0:
       return Promise.resolve(new Track(0, 'Faded - Alan Walker', { youtubeId: '60ItHLz5WEA' }))
-    case 149:
-      return Promise.resolve(new Track(149, 'Cheap Thrills', { youtubeId: 'J1b22l1kFKY' }))
-    case 170:
-      return Promise.resolve(new Track(170, 'Someone Like You', { youtubeId: 'hLQl3WQQoQ0' }))
     case 1:
+      return Promise.resolve(new Track(149, 'Beyoncé - Yoncé', { youtubeId: 'jcF5HtGvX5I' }))
+    case 2:
       return Promise.resolve(new Track(1, 'The Night Is Still Young', { youtubeId: 'IvN5h9BE444' }))
   }
 }
