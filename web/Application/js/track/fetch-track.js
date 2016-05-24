a5b1edcc3670ad3d7e64122b75876b046b3f1948
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-05-24 20:14:32 +0200

    update scrobble bar

--- web/Application/js/track/fetch-track.js
+++ web/Application/js/track/fetch-track.js
@@ -13,7 +13,7 @@ export function fetchTrack(trackId) {
   // TODO: fetch this from the server
   switch(trackId) {
     case 0:
-      return Promise.resolve(new Track(0, 'Faded', 'Alan Walker', { youtubeId: '60ItHLz5WEA' }))
+      return Promise.resolve(new Track(0, 'Faded', 'Alan Walker', { youtubeId: '60ItHLz5WEA', duration: 212 }))
     case 1:
       return Promise.resolve(new Track(149, 'Yoncé', 'Beyoncé', { youtubeId: 'jcF5HtGvX5I' }))
     case 2:
