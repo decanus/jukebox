b8034e0fc39910d54170df80a55513af676d862a
decanus <dean.eigenmann@icloud.com>
2016-05-27 16:54:58 +0200

    nuice

--- web/Framework/src/ValueObjects/DataVersion.php
+++ web/Framework/src/ValueObjects/DataVersion.php
@@ -4,6 +4,30 @@ namespace Jukebox\Framework\ValueObjects
 {
     class DataVersion
     {
+        private $version;
 
+        public function __construct(string $version)
+        {
+            $this->setVersion($version);
+        }
+
+        private function setVersion(string $version)
+        {
+            if ($version == 'now') {
+                $version = new \DateTime();
+                $version = $version->format('Ymd-Hi');
+            }
+
+            if (\DateTime::createFromFormat('Ymd-Hi', $version) === false) {
+                throw new \InvalidArgumentException('Invalid version format "' . $version . '"');
+            }
+
+            $this->version = $version;
+        }
+
+        public function __toString(): string
+        {
+            return (string) $this->version;
+        }
     }
 }
