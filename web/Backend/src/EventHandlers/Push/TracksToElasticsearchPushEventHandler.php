f247632be76f309d0dbb08b1b6721b7da0da6494
decanus <dean.eigenmann@icloud.com>
2016-05-30 19:45:24 +0200

    Updated

--- web/Backend/src/EventHandlers/Push/TracksToElasticsearchPushEventHandler.php
+++ web/Backend/src/EventHandlers/Push/TracksToElasticsearchPushEventHandler.php
@@ -5,17 +5,26 @@ namespace Jukebox\Backend\EventHandlers\Push
 
     use Elasticsearch\Client;
     use Jukebox\Backend\EventHandlers\EventHandlerInterface;
+    use Jukebox\Backend\Events\TracksToElasticsearchPushEvent;
 
     class TracksToElasticsearchPushEventHandler implements EventHandlerInterface
     {
+        /**
+         * @var TracksToElasticsearchPushEvent
+         */
+        private $event;
 
         /**
          * @var Client
          */
         private $client;
 
-        public function __construct(Client $client)
+        public function __construct(
+            TracksToElasticsearchPushEvent $event,
+            Client $client
+        )
         {
+            $this->event = $event;
             $this->client = $client;
         }
 
