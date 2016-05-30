47520d5ed4c947cf25518d351bde2b6cad093892
decanus <dean.eigenmann@icloud.com>
2016-05-30 22:13:17 +0200

    started initial

--- web/Backend/src/EventHandlers/InitialEventHandler.php
+++ web/Backend/src/EventHandlers/InitialEventHandler.php
@@ -2,11 +2,37 @@
 
 namespace Jukebox\Backend\EventHandlers
 {
+
+    use Jukebox\Backend\Events\ArtistsToElasticsearchPushEvent;
+    use Jukebox\Backend\Events\ElasticsearchIndexPushEvent;
+    use Jukebox\Backend\Events\InitialVevoArtistsImportEvent;
+    use Jukebox\Backend\Events\InitialVevoArtistsVideosImportEvent;
+    use Jukebox\Backend\Events\TracksToElasticsearchPushEvent;
+    use Jukebox\Backend\Writers\EventQueueWriter;
+    use Jukebox\Framework\ValueObjects\DataVersion;
+
     class InitialEventHandler implements EventHandlerInterface
     {
+        /**
+         * @var EventQueueWriter
+         */
+        private $eventQueueWriter;
+
+        public function __construct(EventQueueWriter $eventQueueWriter)
+        {
+            $this->eventQueueWriter = $eventQueueWriter;
+        }
 
         public function execute()
         {
+            $dataVersion = new DataVersion('now');
+
+            $this->eventQueueWriter->add(new InitialVevoArtistsImportEvent);
+            $this->eventQueueWriter->add(new InitialVevoArtistsVideosImportEvent);
+            $this->eventQueueWriter->add(new ElasticsearchIndexPushEvent($dataVersion));
+            $this->eventQueueWriter->add(new ArtistsToElasticsearchPushEvent($dataVersion));
+            $this->eventQueueWriter->add(new TracksToElasticsearchPushEvent($dataVersion));
+
             // TODO: Implement execute() method.
         }
     }
