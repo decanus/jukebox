eb1f6e5236405230cb48220908972abb5e9f86c7
decanus <dean.eigenmann@icloud.com>
2016-05-30 23:40:18 +0200

    updated

--- web/API/src/Search/SearchResult.php
+++ web/API/src/Search/SearchResult.php
@@ -10,5 +10,28 @@ namespace Jukebox\API\Search
         {
             $this->response = $response;
         }
+        
+        public function wasFound(): bool
+        {
+            if (!isset($this->response['found'])) {
+                return false;
+            }
+
+            return $this->response['found'];
+        }
+
+        public function hasHits(): bool
+        {
+            return $this->getNumberOfHits() > 0;
+        }
+
+        public function getNumberOfHits(): int
+        {
+            if (!isset($this->response['hits'])) {
+                return 0;
+            }
+
+            return $this->response['hits']['total'];
+        }
     }
 }
