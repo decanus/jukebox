399efc39751a6d73c2f3797118d583e24bb5ca2b
decanus <dean.eigenmann@icloud.com>
2016-06-04 15:53:05 +0200

    Added tracks

--- web/Frontend/src/Handlers/Get/Artist/TransformationHandler.php
+++ web/Frontend/src/Handlers/Get/Artist/TransformationHandler.php
@@ -30,6 +30,28 @@ namespace Jukebox\Frontend\Handlers\Get\Artist
                 $link->setAttribute('href', $artist['permalink']);
                 $link->appendTextNode($artist['name']);
 
+                $tracks = $this->getModel()->getTracks();
+
+                foreach ($tracks as $track) {
+
+                    $recording = $musicGroup->appendElement('article');
+                    $recording->setAttribute('itemprop', 'track');
+                    $recording->setAttribute('itemscope', '');
+                    $recording->setAttribute('itemtype', 'http://schema.org/MusicRecording');
+
+                    $name = $recording->appendElement('h2');
+                    $name->setAttribute('itemprop', 'name');
+
+                    $link = $name->appendElement('a');
+                    $link->setAttribute('itemprop', 'url');
+                    $link->setAttribute('href', $track['permalink']);
+                    $link->appendTextNode($track['title']);
+
+                    $duration = $recording->appendElement('meta');
+                    $duration->setAttribute('itemprop', 'duration');
+                    $duration->setAttribute('content',  'P' . floor($track['duration'] / 1000) . 'S');
+                }
+
             } catch (\Throwable $e) {
                 // @todo log somewhere
             }
