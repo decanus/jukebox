725d2d372962a4243a84a33f1fb47f9621fb75a2
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-06-06 10:45:20 +0200

    update logo, header

--- web/Application/js/elements/toggle-sidebar.js
+++ web/Application/js/elements/toggle-sidebar.js
@@ -3,11 +3,39 @@
  */
 
 import { app } from '../app'
+import { PlayerState } from '../players/player-state'
+
+const player = app.getPlayer()
 
 export class ToggleSidebar extends HTMLButtonElement {
-  createdCallback() {
+  createdCallback () {
     this.addEventListener('click', () => {
       app.toggleSidebar()
     })
+
+    player.getState()
+      .forEach((state) => {
+        this.hidden = (state === PlayerState.STOPPED)
+      })
+  }
+
+  /**
+   *
+   * @param {boolean} hidden
+   */
+  set hidden (hidden) {
+    if (hidden) {
+      this.setAttribute('hidden', 'hidden')
+    } else {
+      this.removeAttribute('hidden')
+    }
+  }
+
+  /**
+   *
+   * @returns {boolean}
+   */
+  get hidden () {
+    return this.hasAttribute('hidden')
   }
 }
