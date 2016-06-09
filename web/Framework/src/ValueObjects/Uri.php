c549f84d3754656631fe669a99af49fed08a845b
decanus <dean.eigenmann@icloud.com>
2016-06-09 19:56:51 +0200

    fix

--- web/Framework/src/ValueObjects/Uri.php
+++ web/Framework/src/ValueObjects/Uri.php
@@ -36,7 +36,7 @@ namespace Jukebox\Framework\ValueObjects
                 parse_str($this->uri['query'], $this->parameters);
             }
 
-            $this->uri['path'] = urldecode($this->uri['path']);
+            $this->uri['path'] = $this->uri['path'];
         }
 
         /**
