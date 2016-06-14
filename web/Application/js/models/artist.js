d78f2a6bb95117b773d4f6856f4feba6d24d824f
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-06-14 21:50:22 +0200

    update artists page

--- web/Application/js/models/artist.js
+++ web/Application/js/models/artist.js
@@ -1,6 +1,8 @@
 /**
  * (c) 2016 Jukebox <www.jukebox.ninja>
  */
+  
+import { parseUrl } from '../url/parse'
 
 export class Artist {
   /**
@@ -8,11 +10,17 @@ export class Artist {
    * @param {number} id
    * @param {string} name
    * @param {string} permalink
+   * @param {string} website
+   * @param {string} facebook
+   * @param {string} twitter
    */
-  constructor ({ id, name, permalink}) {
+  constructor ({ id, name, permalink, website, facebook, twitter }) {
     this.id = id
     this.name = name
     this.permalink = permalink
+    this.website = website
+    this.facebook = facebook
+    this.twitter = twitter
 
     Object.freeze(this)
   }
@@ -31,5 +39,21 @@ export class Artist {
    */
   get isArtist () {
     return true
+  }
+
+  /**
+   * 
+   * @returns {string}
+   */
+  get twitterUrl () {
+    return `https://twitter.com/${this.twitter}`
+  }
+
+  /**
+   * 
+   * @returns {string}
+   */
+  get websiteName () {
+    return parseUrl(this.website).host
   }
 }
