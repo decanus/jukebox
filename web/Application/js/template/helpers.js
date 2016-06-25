fbf5e092ad6ba415cc1baeffb220d36a3b067015
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-06-25 12:08:33 +0200

    oops maybe i should test before push

--- web/Application/js/template/helpers.js
+++ web/Application/js/template/helpers.js
@@ -2,10 +2,8 @@
  * (c) 2016 Jukebox <www.jukebox.ninja>
  */
 
-export const helpers = Object.create(Handlebars.helpers, {
-  json: {
-    value (context) {
-      return JSON.stringify(context)
-    }
+export const helpers = Object.assign({}, Handlebars.helpers, {
+  json (context) {
+    return JSON.stringify(context)
   }
 })
