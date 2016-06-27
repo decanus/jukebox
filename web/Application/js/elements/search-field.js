6144b99981c05e45924d9f7aa54b3d3015551c13
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-06-27 15:49:22 +0200

    wrap search field in form

--- web/Application/js/elements/search-field.js
+++ web/Application/js/elements/search-field.js
@@ -7,20 +7,6 @@ import { Route } from '../value/route'
 
 export class SearchField extends HTMLInputElement {
   createdCallback () {
-    this.addEventListener('keyup', (e) => {
-      if (e.keyCode !== 13) {
-        return
-      }
-
-      let params = new Map()
-
-      if (this.value !== '') {
-        params.set('q', this.value)
-      }
-
-      app.setRoute(new Route('/search', params))
-    })
-
     app.getRouteObservable()
       .forEach((route) => this._updateValue(route))
 
