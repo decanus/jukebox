c600e8f761db5f427cb5198c97b141b01c93e2cd
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-07-04 17:28:07 +0200

    add todo

--- web/Application/js/elements/dialog/dialog-link.js
+++ web/Application/js/elements/dialog/dialog-link.js
@@ -10,6 +10,8 @@ export class DialogLink extends HTMLAnchorElement {
     this.addEventListener('click', (event) => {
       event.preventDefault()
       
+      // todo: enable ctrl-click to open in new tab
+      
       /** @type {DialogContent} */
       const $content = this.ownerDocument.querySelector(`dialog-content#${this.openDialog}`)
 
