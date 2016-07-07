4af7b46afd3c74b8fc76141bef7d6c4398320ed7
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-07-07 21:22:40 +0200

    faster build

--- web/Application/js/jukebox.js
+++ web/Application/js/jukebox.js
@@ -2,7 +2,8 @@
  * (c) 2016 Jukebox <www.jukebox.ninja>
  */
 
-import 'whatwg-fetch'
+import '../node_modules/whatwg-fetch'
+import '../node_modules/regenerator-runtime/runtime'
 
 import { app } from './app'
 import { getInterval } from './dom/time/get-interval'
