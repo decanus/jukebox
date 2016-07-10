c827063ad68d2a76dec1d89fd2bbc6c39a5a0be0
decanus <dean.eigenmann@icloud.com>
2016-07-10 10:40:40 +0200

    updated

--- web/Frontend/src/Handlers/Post/Registration/CommandHandler.php
+++ web/Frontend/src/Handlers/Post/Registration/CommandHandler.php
@@ -7,6 +7,8 @@ namespace Jukebox\Frontend\Handlers\Post\Registration
     use Jukebox\Framework\Http\Request\RequestInterface;
     use Jukebox\Framework\Models\AbstractModel;
     use Jukebox\Framework\ValueObjects\Email;
+    use Jukebox\Framework\ValueObjects\Password;
+    use Jukebox\Framework\ValueObjects\Username;
 
     class CommandHandler implements CommandHandlerInterface
     {
@@ -17,6 +19,20 @@ namespace Jukebox\Frontend\Handlers\Post\Registration
                 $email = new Email($request->getParameter('email'));
             } catch (\Throwable $e) {
                 $model->setData(['error' => 'invalid_email']);
+                return;
+            }
+
+            try {
+                $username = new Username($request->getParameter('username'));
+            } catch (\Throwable $e) {
+                $model->setData(['error' => 'invalid_username']);
+                return;
+            }
+
+            try {
+                $password = new Password($request->getParameter('password'));
+            } catch (\Throwable $e) {
+                $model->setData(['error' => 'invalid_password']);
                 return;
             }
         }
