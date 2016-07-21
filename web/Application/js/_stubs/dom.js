27a78d54c3bc8cb4d2b0b4b4063c0b6c0efc4d49
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-07-21 19:47:42 +0200

    add some js stubs

---
+++ web/Application/js/_stubs/dom.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @param {string} tagName
+ * @param {string} [typeExtension]
+ */
+Document.prototype.createElement = function(tagName, typeExtension) {};
+
+/**
+ * @returns {Range}
+ */
+Document.prototype.createRange = function () {};
