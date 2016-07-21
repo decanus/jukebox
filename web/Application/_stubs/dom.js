b7de9556af427ea0afd4b698878cce533aca8783
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-07-21 20:06:12 +0200

    moved

--- web/Application/_stubs/dom.js
+++ web/Application/_stubs/dom.js
@@ -6,3 +6,15 @@
 Element.prototype.matches = function (selectorString) {}
 
 HTMLDocument.prototype.body = new HTMLBodyElement()
+
+/**
+ *
+ * @param {string} tagName
+ * @param {string} [typeExtension]
+ */
+Document.prototype.createElement = function(tagName, typeExtension) {};
+
+/**
+ * @returns {Range}
+ */
+Document.prototype.createRange = function () {};
