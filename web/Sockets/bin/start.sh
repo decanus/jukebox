0d27e1c94dd2b2882118c8a1bc0b36df94ed2f65
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-07-23 17:27:19 +0200

    fix start.sh

--- web/Sockets/bin/start.sh
+++ web/Sockets/bin/start.sh
@@ -2,8 +2,8 @@
 
 DIR=$(dirname $0)
 
-node "${DIR}/../index.js" --port=8080 --instance=1 &
-node "${DIR}/../index.js" --port=8081 --socketId=2 &
-node "${DIR}/../index.js" --port=8082 --socketId=3 &
+node "${DIR}/../index.js" --instance=0 &
+node "${DIR}/../index.js" --instance=1 &
+node "${DIR}/../index.js" --instance=2 &
 
 wait
