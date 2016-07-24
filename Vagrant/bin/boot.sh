b2a12adeae6ae9a9cff572bd063ab2727590d026
decanus <dean.eigenmann@icloud.com>
2016-07-24 15:43:07 +0200

    ciao mongo

--- Vagrant/bin/boot.sh
+++ Vagrant/bin/boot.sh
@@ -5,7 +5,6 @@ PATH=$PATH:/usr/local/bin
 systemctl start nginx.service
 systemctl start php-fpm.service
 systemctl start redis.service
-systemctl start mongod.service
 systemctl start postgresql.service
 systemctl start elasticsearch.service
 systemctl start jn-mirror-sockets.service
